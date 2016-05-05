bb3700a09751dfbd4dfd55f5797356340a690ea9
MangoFang <100mango@gmail.com>
2016-05-05 15:26:45 +0800

    init

---
+++ SwiftNotificationCenter/SwiftNotificationCenter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNotificationCenter.h
+//  SwiftNotificationCenter
+//
+//  Created by Mango on 16/5/5.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNotificationCenter.
+FOUNDATION_EXPORT double SwiftNotificationCenterVersionNumber;
+
+//! Project version string for SwiftNotificationCenter.
+FOUNDATION_EXPORT const unsigned char SwiftNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNotificationCenter/PublicHeader.h>
+
+
---
+++ SwiftNotificationCenterTests/SwiftNotificationCenterTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftNotificationCenterTests.swift
+//  SwiftNotificationCenterTests
+//
+//  Created by Mango on 16/5/5.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftNotificationCenter
+
+class SwiftNotificationCenterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
